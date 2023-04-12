e8722c1e5f51504178ae0a533c1b19d0b46b3492
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-04-12 11:35:27 -0600

    Add scopes reporting methods to create page resumes

--- app/models/page_load.rb
+++ app/models/page_load.rb
@@ -1,13 +1,38 @@
 class PageLoad < ApplicationRecord
-  scope :last_month, -> { where('created_at > ?', 1.month.ago) }
-  scope :last_week, -> { where('created_at > ?', 1.week.ago) }
-  scope :last_hour, -> { where('created_at > ?', 1.hour.ago) }
-  scope :yesterday, -> { where('DATE(created_at) = ?', 1.day.ago.to_date) }
-  scope :today, -> { where('DATE(created_at) = ?', Date.today) }
+  scope :per_minute, -> { time_bucket('1 minute') }
+  scope :per_hour, -> { time_bucket('1 hour') }
+  scope :per_day, -> { time_bucket('1 day') }
+  scope :per_week, -> { time_bucket('1 week') }
+  scope :per_month, -> { time_bucket('1 month') }
+  scope :average_response_time_per_minute, -> { time_bucket('1 minute', value: 'avg(performance)') }
+  scope :average_response_time_per_hour, -> { time_bucket('1 hour', value: 'avg(performance)') }
+  scope :worst_response_time_last_minute, -> { time_bucket('1 minute', value: 'max(performance)') }
+  scope :worst_response_time_last_hour, -> { time_bucket('1 hour', value: 'max(performance)') }
+  scope :best_response_time_last_hour, -> { time_bucket('1 hour', value: 'min(performance)') }
+  scope :paths, -> { distinct.pluck(:path) }
+  scope :time_bucket, -> (time_dimension, value: 'count(1)') {
+    select(<<~SQL)
+      time_bucket('#{time_dimension}', created_at) as time, path,
+      #{value} as value
+    SQL
+     .group('time, path').order('path, time')
+    }
 
-  scope :counts_per, -> (time_dimension) {
-    select("time_bucket('#{time_dimension}', created_at) as time, count(1) as total")
-    .group(:time).order(:time)
-      .map {|result| [result.time, result.total]}
-  }
+  def self.resume_for(path)
+   filter = where(path: path)
+   get = -> (scope_name) { filter.send(scope_name).first&.value}
+   metrics.each_with_object({}) do |metric, resume|
+       resume[metric] = get[metric]
+   end
+  end
+
+  def self.metrics
+    methods.grep /response_time/
+  end
+
+  def self.statistics
+    paths.each_with_object({}) do |path, resume|
+      resume[path] = resume_for(path)
+    end
+  end
 end
